f3b23755545aa2fb7796877a43454dfa8d1f0cc4
WraasTheOne <113178081+WraasTheOne@users.noreply.github.com>
2023-02-07 10:48:15 +0100

    Gui

---
+++ Gui.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Gui {
+    private JRadioButton radioButton1;
+    private JRadioButton radioButton2;
+    private JTextField textField1;
+    private JCheckBox checkBox1;
+}
