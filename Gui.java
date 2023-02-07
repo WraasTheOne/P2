2e960db7516369bc2874246d66fbd95df8db78f1
WraasTheOne <113178081+WraasTheOne@users.noreply.github.com>
2023-02-07 16:26:40 +0100

    Basic UI window made //M

--- Gui.java
+++ Gui.java
@@ -1,12 +1,5 @@
 import javax.swing.*;
 
 public class Gui extends JFrame{
-    private JPanel Main_panel;
-    private JLabel Label_1;
-
-    public static void main(){
-        Gui frame = new Gui();
-
-
-    }
+    public JPanel mainPanel;
 }
--- Main.java
+++ Main.java
@@ -2,8 +2,12 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        Gui gui = new Gui();
+        Gui Frame = new Gui();
+        Frame.setContentPane(Frame.mainPanel);
+        Frame.setTitle("GUI demo");
+        Frame.setSize(420,420);
+        Frame.setVisible(true);
+        Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
     }
