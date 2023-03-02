19b78f30b15bcb0f0a11f48fe7720f2c199e815e
WraasTheOne <113178081+WraasTheOne@users.noreply.github.com>
2023-03-02 21:50:41 +0100

    Start på at lege med login system

--- App/src/main/java/com/example/app/HelloApplication.java
+++ App/src/main/java/com/example/app/HelloApplication.java
@@ -12,6 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 420, 420);
+        scene.getStylesheets().add("com/example/app/stylesheet.css");
         stage.setTitle("P2 - app");
         stage.setScene(scene);
         stage.show();
