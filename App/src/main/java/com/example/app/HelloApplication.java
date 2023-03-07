c953b52a2930f08c959764e68a2f02da875ebb2a
WraasTheOne <113178081+WraasTheOne@users.noreply.github.com>
2023-03-07 17:42:02 +0100

    har eksperimenteret med charts

--- App/src/main/java/com/example/app/HelloApplication.java
+++ App/src/main/java/com/example/app/HelloApplication.java
@@ -1,23 +1,29 @@
 package com.example.app;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.Chart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
-        Scene scene = new Scene(new Pane(),420,420);
+        Scene scene = new Scene(new Pane(),900,420);
 
         ViewSwitch.setScene(scene);
-        ViewSwitch.switchView(View.LOGIN);
+        ViewSwitch.switchView(View.LoggedIn);
         //stage.setMaximized(true);
         stage.setScene(scene);
-        stage.setTitle("SagaLabs-Manager");
+        stage.setTitle("P2 - app");
         stage.show();
     }
 
@@ -25,7 +31,4 @@ public class HelloApplication extends Application {
         launch();
     }
 
-    public void viewSwitch(){
-
-    }
 }
--- App/src/main/java/com/example/app/LoggedInController.java
+++ App/src/main/java/com/example/app/LoggedInController.java
@@ -1,6 +1,8 @@
 package com.example.app;
 
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
 
 import java.io.IOException;
@@ -11,17 +13,30 @@ public class LoggedInController {
 
     @FXML
     private Button Button2;
-
+    @FXML
+    private BarChart<String, Integer> chart;
     @FXML
     private Label Label1;
 
     @FXML
     protected void buttonCliked() throws IOException {
         System.out.println("HEJ");
-        Label1.setText("Welcome to JavaFX Application!");
-        Button1.setText("hej");
-        Button2.setText("ad");
-        ViewSwitch.switchView(View.HELLO);
-        
+        //Label1.setText("Welcome to JavaFX Application!");
+        //Button1.setText("hej");
+        //Button2.setText("ad");
+        //ViewSwitch.switchView(View.HELLO);
+
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setName("test");
+        series1.getData().add(new XYChart.Data<>("Test1",17));
+        series1.getData().add(new XYChart.Data<>("Test2",10));
+
+        XYChart.Series series2 = new XYChart.Series();
+        series2.setName("test");
+        series2.getData().add(new XYChart.Data<>("Test1",130));
+        series2.getData().add(new XYChart.Data<>("Test2",75));
+
+        chart.getData().addAll(series1,series2);
+
     }
 }
