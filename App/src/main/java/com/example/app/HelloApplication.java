8546ea64350668784f2654d0e6dde5b399b293b5
Nierfeldt <oliver@nierfeldt.dk>
2023-03-17 10:26:40 +0100

    første push

--- App/src/main/java/com/example/app/HelloApplication.java
+++ App/src/main/java/com/example/app/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MakeCode.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 420, 420);
         stage.setTitle("P2 - app");
         stage.setScene(scene);
