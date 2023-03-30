08c8a2011af5b5a6ab7a04d133e2666b42971150
WraasTheOne <113178081+WraasTheOne@users.noreply.github.com>
2023-03-30 15:13:32 +0200

    db kan connectes til, og kan hente info fra tables

--- App/src/main/java/com/example/app/DBUtil.java
+++ App/src/main/java/com/example/app/DBUtil.java
@@ -1,11 +1,9 @@
 package com.example.app;
 
 
-
 import io.github.cdimascio.dotenv.Dotenv;
 
 import java.sql.*;
-import java.util.Objects;
 
 public class DBUtil {
 
@@ -15,13 +13,24 @@ public class DBUtil {
         String url = dotenv.get("DATABASE_MYSQL_AZURE");
         String User = dotenv.get("USER_MYSQL_AZURE");
         String Pasword = dotenv.get("PASSWORD_MYSQL_AZURE");
+        Statement statement = null;
 
         try {
             myDbConn = DriverManager.getConnection(url, User, Pasword);
+            statement = myDbConn.createStatement();
         }catch (SQLException ex){
             System.out.println("prut "+ ex);
         }
 
+        String test = "select * from users";
+
+
+        System.out.println(statement);
+        ResultSet resultSet = statement.executeQuery(test);
+        System.out.println(resultSet);
+        while (resultSet.next()){
+            System.out.println(resultSet.getString("name"));
+        }
 
 
     }
