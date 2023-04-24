fa780a73942b9d2bf732374c42620039bfb0a71f
WraasTheOne <113178081+WraasTheOne@users.noreply.github.com>
2023-04-24 14:38:32 +0200

    test

--- App/src/main/java/com/example/app/TableStructure/Kooperation.java
+++ App/src/main/java/com/example/app/TableStructure/Kooperation.java
@@ -14,13 +14,10 @@ public class Kooperation extends User implements bigbagInterface{
 
     @Override
     public void changeProcess(String procesChangeTo, BigBag bigbag) {
-        int tidProcess = DBUtil.getsingleValue("bigbags",1);
+        int tidProcess = DBUtil.getsingleValue("bigbags",bigbag.getBID());
         int newProcess = HashTable.getProcessHashValue(procesChangeTo);
-        DBUtil.copyColumnValue("bigbags",tidProcess,1);
-        DBUtil.updateTimeForBigbag(1);
-        BigBag.setBID(1);
-        System.out.println(BigBag.getBID());
-        BigBag.setNUVProcess(newProcess);
+        DBUtil.copyColumnValue("bigbags",tidProcess,bigbag.getBID());
+        DBUtil.updateTimeForBigbag(bigbag.getBID());
     }
 
 }
