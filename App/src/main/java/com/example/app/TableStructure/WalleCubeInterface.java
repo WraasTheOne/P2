5f4e39e2387521b526c92e24f46e50a6d18209bb
yamoc <yam.oceans@protonmail.com>
2023-05-04 11:34:37 +0200

    Something, mate

---
+++ App/src/main/java/com/example/app/TableStructure/WalleCubeInterface.java
@@ -0,0 +1,13 @@
+package com.example.app.TableStructure;
+
+public interface WalleCubeInterface
+{
+    String createWalleCube();
+
+    String removeWalleCube();
+
+    String changeCubeType();
+
+    String changeCenterID();
+
+}
